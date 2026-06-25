4ea7cf43f1ec6c3bfa937c705fbb63033682c6f8
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2026-06-25 16:32:44 -0400

    haven't find parameters

---
+++ build/direct_plot.py
@@ -0,0 +1,144 @@
+# this file reads the output file and slip history file and plots them
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
+if __name__ == "__main__":
+
+    folder = "/home/lx4572/Desktop/research/RSQSim/results/test_RSQSim/test_6/"
+    params = json.load(open(folder + 'parameters.json'))
+    param_r, param_e, param_m = params['region'], params['elastic'], params['model']
+    my, nx, dx, dy = param_r['my'], param_r['nx'], param_r['dx'], param_r['dy']
+    Dc, Vpl = param_e['D_c'], param_e['V_pl']
+
+    istep_record = param_m['step_record']
+
+    out_file = pd.read_csv(folder + "out_file.csv")
+    times = pd.read_csv(folder + "times.csv")
+
+    with np.load(folder + 'slip_plot.npz') as data:
+        slip_time = data['slip_time']
+        slip_plot = data['slip_plot']
+        stress_plot = data['stress_plot']
+    iplot = len(slip_time)
+
+    # plot slip plots
+    plt.figure()
+    for i in range(0, iplot, 1000):
+        plt.plot(np.arange(nx)*dx/1000, slip_plot[i, :]*Dc*100)
+    plt.xlabel('X (km)')
+    plt.ylabel('slip distance (cm)')
+    plt.tight_layout()
+    plt.savefig(folder + 'slip.png', dpi=300)
+    print('slip profile saved')
+
+    # plot stress plots
+    plt.figure()
+    for i in range(0, iplot, 1000):
+        plt.plot(np.arange(nx)*dx/1000, stress_plot[i, :])
+    plt.xlabel('X (km)')
+    plt.ylabel('Stress')
+    plt.tight_layout()
+    plt.savefig(folder + 'stress.png', dpi=300)
+    print('stress profile saved')
+
+    # plot moment rate figures
+    plt.subplots(2, 2, figsize=(10, 8))
+    ax = plt.subplot(2, 2, 1)
+    ax.plot(times['time'], out_file['slip_number'])
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Number of slip elements')
+    ax = plt.subplot(2, 2, 2)
+    ax.plot(range(0, len(out_file)), out_file['slip_number'])
+    ax.set_xlabel('Step')
+    ax.set_ylabel('Number of slip elements')
+    ax = plt.subplot(2, 2, 3)
+    ax.plot(times['time'], out_file['nucleation_number'])
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Number of nucleation elements')
+    ax = plt.subplot(2, 2, 4)
+    ax.plot(range(0, len(out_file)), out_file['nucleation_number'])
+    ax.set_xlabel('Step')
+    ax.set_ylabel('Number of nucleation elements')
+    plt.tight_layout()
+    plt.savefig(folder + 'slip_nucleation.png', dpi=300)
+    print('slip and nucleation plot saved')
+
+    # plot the animation of events
+    plot_animation = True
+    if plot_animation:
+        # Define your colormap and normalization
+        cmap = ListedColormap(["white", "#ecb78d", "red"])
+        norm = BoundaryNorm([-0.5, 0.5, 1.5, 2.5], cmap.N)
+
+        # Your timestep range
+        timestep_range = np.arange(11000, 12000, 1)  # Adjust based on your timestep range
+        ani_time = slip_time[timestep_range]
+        ani_stress = stress_plot[timestep_range]
+        ani_slip = slip_plot[timestep_range]
+        ani_state = np.zeros((len(timestep_range), my, nx), dtype=np.int8)
+        state_plot = np.zeros((my, nx), dtype=np.int8)
+        i_timestep = 0
+        for i in range(iplot):
+            j = int(out_file['jj'].iloc[i])
+            k = int(out_file['kk'].iloc[i])
+            if out_file['index'].iloc[i] == 2:
+                state_plot[j, k] = 2
+            elif out_file['index'].iloc[i] == 1:
+                state_plot[j, k] = 1
+            else:
+                state_plot[j, k] = 0
+
+            if i == timestep_range[i_timestep]:
+                ani_state[i_timestep] = state_plot
+                i_timestep += 1
+                if i_timestep == len(timestep_range):
+                    break
+
+
+        # Create a figure and axes for the plots
+        fig, axs = plt.subplots(3, 1, figsize=(8, 8), sharex=True, gridspec_kw={'height_ratios': [1, 1, 2]})
+
+        # Plot elements
+        stress_line, = axs[0].step([], [], where='post', c="tab:blue")
+        slip_line, = axs[1].step([], [], where='post', c="tab:green")
+        state_imshow = axs[2].imshow(np.zeros((my, nx)), aspect='auto', cmap=cmap, norm=norm, origin="lower", extent=[0, nx*dx/1000, 0, my*dy/1000])
+        title = fig.suptitle(f"Time = {slip_time[1]*Dc/Vpl/3600:.2f} h", fontsize=12)
+
+        # Adjust axes labels
+        axs[0].set_xlim(0, 6)
+        axs[0].set_ylim(-2, 6)
+        axs[0].set_ylabel('Normalized stress ' + r'$\tau / \tau_p$')
+        axs[1].set_ylabel('Slip distance (cm)')
+        axs[1].set_ylim(0, 800)
+        axs[2].set_xlabel('Location along strike (km)')
+        axs[2].set_ylabel('Location along dip (km)')
+
+        plt.tight_layout()
+
+        # Update function to modify plot elements at each timestep
+        def update(timestep):
+            # Update stress plot
+            stress_line.set_data(np.arange(nx)*dx/1000, ani_stress[timestep])
+            
+            # Update slip plot
+            slip_line.set_data(np.arange(nx)*dx/1000, ani_slip[timestep] * 100 * Dc)
+            
+            # Update state plot
+            state_imshow.set_data(ani_state[timestep])
+
+            # Update title
+            title.set_text(f"Time = {(ani_time[timestep]*Dc/Vpl)/3600:.2f} h")
+            
+            return stress_line, slip_line, state_imshow
+
+        # Create the animation
+        ani = FuncAnimation(fig, update, frames=range(0, len(timestep_range)), interval=250, blit=True)
+
+        # To save the animation as a video (optional)
+        ani.save(folder + 'animation.mp4', writer='ffmpeg', fps=20)
--- build/simulator_v2.py
+++ build/simulator_v2.py
@@ -10,6 +10,8 @@
 # 2. where to start the initiation
 # ---------------- update date: 20 June
 
+# The first question has been answered:
+#   The kernel matrix should be normalized by b\sigma
 
 import numpy as np
 import pandas as pd
@@ -29,11 +31,8 @@ def prep(folder):
 
     dx, dy, nx, my = param_r['dx'], param_r['dy'], param_r['nx'], param_r['my']
     xper, yper, sourcenum = param_r['xper'], param_r['yper'], param_r['sourcenum']
-    W, hh, mu_d, mudomu = param_r['W'], param_r['hh'], param_r['mu_d'], param_r['mudomu']
-    if W != 0:
-        G, D = mu_d/mudomu, 1-mudomu
-    else:
-        G, D = mu_d, 0
+    W, hh, G, mu_d = param_r['W'], param_r['hh'], param_r['G'], param_r['mu_d']
+    D = 1 - mu_d / G if W != 0 else 0
 
     Kjk = comp_kernel.compute(W, hh, G, D, xper, yper, nx, my, dx, dy, sourcenum)
 
@@ -41,71 +40,70 @@ def prep(folder):
     Vpl = param_e['V_pl']
     loading = 'bs+'
     taudot = loadrate.loadrate(Vpl, loading, hh, W, mu_d, G, my, nx, Kjk)
-    
-    # peak strength 
-    tau_p0 = param_e['tau_p0']
-    tau_p = np.ones((my, nx), dtype=np.float64) * tau_p0
 
+    # dimensionalize Kjk and taudot
+    b, sigma, Dc = param_e['b'], param_e['sigma'], param_e['D_c']
+    Kjk = Kjk * Dc / (b * sigma)
+    taudot = taudot * Dc / Vpl / (b * sigma)
+    
     # initialize stress
-    mu_dist = np.log(tau_p0)
     np.random.seed(1)
-    sigma_dist = param_e['sigma_dist']
-    tau = 0.25 * lognorm(s=sigma_dist, scale=np.exp(mu_dist)).rvs((my, nx))
+    Dtau_0, sigma_dist = param_e['Dtau_0'], param_e['sigma_dist']
+    # tau = -0.01 + 0.25 * lognorm(s=sigma_dist, scale=Dtau_0).rvs((my, nx))
+    tau = np.random.normal(loc=-2, scale = 0.005, size=(my, nx))
 
     # update and save the parameters
-    param_e['Kii'] = Kjk[0, 0]
-    param_r['G'], param_r['D'], param_r['Axy'] = G, D, dx * dy
+    param_e['Kii'], param_r['Axy'] = Kjk[0, 0], dx * dy
     parameters['elastic'], parameters['region'] = param_e, param_r
     with open(folder + 'parameters.json', 'w') as json_file:
         json.dump(parameters, json_file, indent=4)
 
-    return Kjk, taudot, tau_p, tau
+    return Kjk, taudot, tau
 
-def initiate():
+def initiate(param_directory):
+    my, nx = param_directory['my'], param_directory['nx']
+    Veq_n = param_directory['Veq_n']
     indx = np.zeros((my, nx), dtype=np.int64)
-    tau = np.ones((my, nx), dtype=np.float64)*6
     velocity = np.zeros((my, nx), dtype=np.float64)
-    q = np.zeros((my, nx), dtype=np.float64)
+    q = np.ones((my, nx), dtype=np.float64) / Veq_n
 
-    return indx, tau, velocity, q
+    return indx, velocity, q
 
 # @njit # (parallel=True)
-def update_step(indx, tau, slip, Vslip, taudot, Kij, Kii, my, nx):
+def update_step(indx, Dtau, taudot, velocity, q, Kjk, param_directory, slip):
 
-    # some artifial values
-    aob = 0.8
-    omaob = 1 - aob
-    Veq = 1e3
-    Kii = -0.6
-    omKii = 1 - Kii
-    overshoot = 0.1
-    Dtaupbg = overshoot * 6
-    Dtaupmin = 0.01
+    # read in the parameters
+    my, nx = param_directory['my'], param_directory['nx']
+    overshoot, Dtaupmin = param_directory['overshoot'], param_directory['Dtaupmin']
+    aob, omaob = param_directory['aob'], param_directory['omaob']
+    omKii = param_directory['omKii']
+    Veq_n = param_directory['Veq_n']  
 
+    Dtaupmin = -2
     # determine the next time step and the element that will transition
     dtnext = 1e300
     jj = 0
     kk = 0
     
     Dt = np.zeros((my, nx), dtype=np.float64)
-    Dtaup = np.ones((my, nx), dtype=np.float64) * Dtaupbg
+    Dtaup = np.ones((my, nx), dtype=np.float64) * Dtaupmin
     for j in range(my):
         for k in range(nx):
             if indx[j, k] == 0:
                 Dttest = 0.0
-                Dt[j, k] = ((omaob * np.log(Veq*q[j,k])) - tau[j,k]) / taudot[j,k]
+                Dt[j, k] = ((omaob * np.log(q[j,k])) - Dtau[j,k]) / taudot[j,k]
                 while abs(Dt[j, k] - Dttest) > 1e-5 * abs(Dt[j, k]):
                     Dttest = Dt[j, k]
-                    Dt[j, k] = ((omaob * np.log(Veq*(q[j,k]+Dttest))) - tau[j,k]) / taudot[j,k]
+                    Dt[j, k] = ((omaob * np.log(q[j,k]+Dttest)) - Dtau[j,k]) / taudot[j,k]
                 Dt[j, k] = max(0, Dt[j, k])
             elif indx[j, k] == 1:              
-                Dt[i] = -(aob / taudot[i]) * np.log(
-                    ((1.0 / Veq) + omKii / taudot[i]) / 
-                    ((1.0) / velocity[i] + omKii / taudot[i])
+                Dt[j,k] = -(aob / taudot[j,k]) * np.log(
+                    ((1.0 / Veq_n) + omKii / taudot[j,k]) / 
+                    ((1.0) / velocity[j,k] + omKii / taudot[j,k])
                 )
-                Dt[i] = max(0, Dt[i])
+                Dt[j,k] = max(0, Dt[j,k])
             else: # index == 2
-                Dt[j, k] = (-Dtaup[j, k] - tau[j, k]) / taudot[j, k]
+                Dt[j, k] = (Dtaup[j, k] - Dtau[j, k]) / taudot[j, k]
                 Dt[j, k] = max(0, Dt[j, k])
             
             if Dt[j, k] < dtnext:
@@ -127,63 +125,68 @@ def update_step(indx, tau, slip, Vslip, taudot, Kij, Kii, my, nx):
     for j in range(my):
         for k in range(nx):
 
-            tau[j,k] += dtnext * taudot[j,k]
+            Dtau[j,k] += dtnext * taudot[j,k]
 
             if indx[j, k] == 0:
                 if (j, k) == (jj, kk):
-                    q[j,k] = 1 / Veq
+                    q[j,k] = 1 / Veq_n
+                    slip[j,k] += Veq_n * dtnext
                 else:
                     q[j,k] += dtnext
             elif indx[j, k] == 1:
                 if (j, k) == (jj, kk):
-                    velocity[j,k] = 1.0 / (q[i] + dtnext)
+                    velocity[j,k] = 1.0 / (q[j,k] + dtnext)
                 else:
                     V0m1 = (
-                        (1.0 / velocity[i] + omKii / taudot[i])
-                        * np.exp(-taudot[i] * dtnext / aob)
-                        - omKii / taudot[i]
+                        (1.0 / velocity[j,k] + omKii / taudot[j,k])
+                        * np.exp(-taudot[j,k] * dtnext / aob)
+                        - omKii / taudot[j,k]
                     )
                     velocity[j,k] = 1.0 / V0m1
             else:
                 if (j, k) == (jj, kk):
-                    Dtaup[j,k] = max(Dtaupmin, overshoot*Dtaup[j,k])
+                    Dtaup[j,k] = min(Dtaupmin, - overshoot*Dtaup[j,k])
                 else:
-                    slip[j,k] += Vslip * dtnext
-                q[i] = 1.0 / Veq
+                    slip[j,k] += Veq_n * dtnext
+                q[j,k] = 1.0 / Veq_n
         
-            taudot[j,k] += ico * Veq * Kij[abs(jj-j), abs(kk-k)]
+            taudot[j,k] += ico * Veq_n * Kjk[abs(jj-j), abs(kk-k)]
 
-    return dtnext, jj, kk, tau, taudot, indx, slip
+    return dtnext, jj, kk, Dtau, taudot, indx, velocity, q, slip
 
 if __name__ == "__main__":
 
-    folder = '../results/test_RSQSim/test_4/'
+    folder = '../results/test_RSQSim/test_6/'
     if not os.path.exists(folder + 'events/'):
         os.makedirs(folder + 'events/')
     
     # prepare the kernel function and stress
     start = time.time()
-    Kjk, taudot, tau_p, tau = prep(folder)
-    # K_vert = np.concatenate((Kjk[::-1, :], Kjk[1:, :]), axis=0)
-    # K_sym = np.concatenate((K_vert[:, ::-1], K_vert[:, 1:]), axis=1)
+    Kjk, taudot, Dtau = prep(folder)
     end = time.time()
     print(f"time for preparation:{end-start :.4f} seconds")
 
     params = json.load(open(folder + 'parameters.json'))
     param_r, param_e, param_m = params['region'], params['elastic'], params['model']
-    eps_D, Vslip = param_e['eps_D'], param_e['Vslip']
-    dx, dy, my, nx, Axy = param_r['dx'], param_r['dy'], param_r['my'], param_r['nx'], param_r['Axy']
+    Veq, Vpl, my, nx = param_e['V_eq'], param_e['V_pl'], param_r['my'], param_r['nx']
+    a, b, overshoot, Dtaupmin = param_e['a'], param_e['b'], param_e['overshoot'], param_e['Dtaupmin']
+    aob, omaob = a/b, 1 - a/b
+    omKii = 1 - abs(Kjk[0, 0])
+    Veq_n = Veq / Vpl  # non-dimensionalize the velocity
+
+    param_directory = {'my': my, 'nx':nx, 'aob': aob, 'omaob': omaob, 'omKii': omKii, 'Veq_n': Veq_n, 'Dtaupmin': Dtaupmin, 'overshoot': overshoot}
 
     # initiation inside the simulation
     istep_record = param_m['step_record']
     tim = 0.0
+    nucleation_number = 0
     slip_number = 0
     istep = 0
 
-    indx, tau, velocity, q = initiate()
+    indx, velocity, q = initiate(param_directory)
 
     # initiation about plotting the slip profile
-    len_slip = 500000
+    len_slip = 50000
     slip = np.zeros((my, nx))
     slip_plot = np.zeros((len_slip, nx))
     stress_plot = np.zeros((len_slip, nx))
@@ -191,67 +194,59 @@ if __name__ == "__main__":
     iplotslip = 0
 
     # final output files
-    # outfile: 0: time, 1: jj, 2: kk, 3: index(after trnsition), 4: number_1
-    outfile_np = np.zeros((istep_record, 5))
+    # outfile: 0: time, 1: jj, 2: kk, 3: index(after trnsition), 4: number_2, 5: number_1
+    times_np = np.zeros(istep_record, dtype=np.float64)
+    outfile_np = np.zeros((istep_record, 5), dtype=np.int32)
 
     start = time.time()
     while istep < istep_record:
 
-        # I feel like the initiation should be put here
-        if slip_number == 0:
-            tau_s = tau_p.copy()
+        # # I feel like something should change if one big characteristic event finishes
+        # if slip_number == 0:
+        #     continue 
 
-        dtnext, jj, kk, tau, taudot, indx, slip = update_step(indx, tau, slip, Vslip, taudot, Kjk, my, nx)
+        dtnext, jj, kk, Dtau, taudot, indx, velocity, q, slip = update_step(indx, Dtau, taudot, velocity, q, Kjk, param_directory, slip)
         
         tim += dtnext
 
-        outfile_np[istep, 0] = tim
-        outfile_np[istep, 1] = jj
-        outfile_np[istep, 2] = kk
-        outfile_np[istep, 3] = indx[jj, kk]
-        outfile_np[istep, 4] = slip_number
-
+        times_np[istep] = tim
+        outfile_np[istep, 0] = jj
+        outfile_np[istep, 1] = kk
+        outfile_np[istep, 2] = indx[jj, kk]
+        outfile_np[istep, 3] = slip_number # the number of elements that are at state 2
+        outfile_np[istep, 4] = nucleation_number # the number of elements that are at state 1
         
-        if indx[jj, kk] == 1:
+        if indx[jj, kk] == 2:
             slip_number += 1
+            nucleation_number -= 1
+        elif indx[jj, kk] == 1:
+            nucleation_number += 1
         else:
             slip_number -= 1
         
         if dtnext < 0:
             print('Wrong!')
 
-        if (kk == nx/2) & (jj == 10):
-            slip_plot[iplotslip] = np.mean(slip, axis=0)
-            stress_plot[iplotslip] = np.mean(tau, axis=0)
-            iplotslip += 1
-            slip_time[iplotslip] = tim
-        elif istep % int(istep_record/40) == 0:
-            slip_plot[iplotslip] = np.mean(slip, axis=0)
-            stress_plot[iplotslip] = np.mean(tau, axis=0)
-            iplotslip += 1
-            slip_time[iplotslip] = tim
+        slip_plot[iplotslip] = np.mean(slip, axis=0)
+        stress_plot[iplotslip] = np.mean(Dtau, axis=0)
+        slip_time[iplotslip] = tim
+        iplotslip += 1
         
         istep += 1
-        if istep % 2000000 == 0:
+        if istep % 1000 == 0:
             print('istep:', istep)
     
     end = time.time()
     print(f"time for the loop:{end-start :.4f} seconds")
 
     # postprocess and save the plots
-
-    out_file = pd.DataFrame(outfile_np, columns=['time', 'jj', 'kk', 'index', 'number_1'])
+    times = pd.DataFrame(times_np, columns=['time'])
+    out_file = pd.DataFrame(outfile_np, columns=['jj', 'kk', 'index', 'slip_number', 'nucleation_number'])
+    times.to_csv(folder + 'times.csv', index=False)
     out_file.to_csv(folder + 'out_file.csv', index=False)
 
     slip_plot = slip_plot[:iplotslip]
     stress_plot = stress_plot[:iplotslip]
     slip_time = slip_time[:iplotslip]
     np.savez(folder + 'slip_plot.npz', slip_time = slip_time, slip_plot = slip_plot, stress_plot=stress_plot)
-    plt.figure()
-    for i in range(0, iplotslip, 2):
-        plt.plot(np.arange(nx)*param_r['dx']/1000, slip_plot[i, :]*100)
-    plt.xlabel('X (km)')
-    plt.ylabel('slip distance (cm)')
-    plt.tight_layout()
-    plt.savefig(folder + 'slip.png', dpi=300)
 
