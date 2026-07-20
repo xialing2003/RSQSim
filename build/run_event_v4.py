1ae6039cecd6fc352076500eb4eae33708901162
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2026-07-20 11:44:35 -0400

    run event

--- build/run_event_v4.py
+++ build/run_event_v4.py
@@ -1,3 +1,4 @@
+# totally based on run_event_v3.py, the difference is that I generate the event catalog first and then run the propagation profile
 # this file is based on run_event_v2.py
 # the major difference is that events is defined as the period from one element starts slipping no elements are slipping
 # The purpose of this file is to generate the events figure based on the newly developed simulator_v2.py
