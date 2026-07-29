098d8143c57f3d29cfcf3e6a025d82660c6ccdd3
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2026-07-29 14:39:56 -0400

    update prop2

--- build/propagation_v2.py
+++ build/propagation_v2.py
@@ -7,6 +7,8 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from cmcrameri import cm 
+from pathlib import Path
+import pandas as pd
 
 def event_length(folder, eve_num):
     potency = np.load(folder + f'events/potency_{eve_num}.npy')
@@ -59,39 +61,36 @@ def cal_base(folder, coefficient):
 
     return my*nc*Vslip*Axy*coefficient
     
-def plot_event(ax_main, ax_moment, folder, eve_num, vmax, time_window, dis_window, normalize, ene_level, arrow, arrow_0, baseline):
-    event_history = np.load(folder + f'events/history_X_{eve_num}.npy')
-    start_day = 401
-    end_day = 408
-    start_window = int(start_day*(24*60*60)/time_window)
-    end_window = int(end_day*(24*60*60)/time_window)
-    event_history_filter = np.copy(event_history[start_window:end_window, :])
+def plot_event(ax_main, event_history, time_window, dis_window, normalize, arrow, cutoff, start_day, end_day):
+    
+    if cutoff:
+        start_window = int(start_day*(24*60*60)/time_window)
+        end_window = int(end_day*(24*60*60)/time_window)
+        event_history_filter = np.copy(event_history[start_window:end_window, :])
+        start_axis = 0
+        end_axis = end_day-start_day
+    else:
+        event_history_filter = np.copy(event_history)
+        start_axis = 0
+        end_axis = event_history_filter.shape[0]*time_window/(24*60*60)
+    del event_history
 
     mask = event_history_filter > 0
     new_list_t, new_list_x = np.nonzero(mask)
     new_list_n = event_history_filter[mask]/normalize
+    vmax = min(1, np.max(new_list_n) * 0.8)
 
-    if np.any(new_list_n > 1.0000001):
+    if np.any(new_list_n > 1.001):
         print("There is at least one value > 1", np.max(new_list_n))
 
     new_list_t = new_list_t*time_window/(24*60*60)
     new_list_x = new_list_x*dis_window/1000
-
-    potency = np.load(folder + f'events/potency_{eve_num}.npy')
-    
-    pottime = potency[0, :]/(24*60*60)
-    potrate = potency[1, :]
-    mask_pot = (pottime >= start_day) & (pottime <= end_day)
-    
-    pottime = pottime[mask_pot]
-    potrate = potrate[mask_pot]
-
-    coefficient = G*1e6/10**ene_level
+    del event_history_filter
 
     cmap = cm.batlow
-    sc = ax_main.scatter(new_list_t, new_list_x, cmap=cmap, vmin=0, vmax=vmax/normalize, c=new_list_n, s=0.05, rasterized=True)
-    ax_main.xaxis.set_visible(True)
-    ax_main.set_xlim(0, end_day-start_day)
+    sc = ax_main.scatter(new_list_t, new_list_x, cmap=cmap, vmin=0, vmax=vmax, c=new_list_n, s=0.05, rasterized=True)
+    ax_main.xaxis.set_visible(False)
+    ax_main.set_xlim(start_axis, end_axis)
     ax_main.set_ylim(-0.5, 90)
     ax_main.spines['bottom'].set_visible(False)
     ax_main.spines['top'].set_visible(False)
@@ -114,65 +113,92 @@ def plot_event(ax_main, ax_moment, folder, eve_num, vmax, time_window, dis_windo
             xytext=(start_day, start_x+10), 
             arrowprops=dict(arrowstyle='-|>', color='tab:green', lw=2, linestyle='--', mutation_scale=15)
         )
-        ax_main.text(start_day+1, start_x+10+Vprop_simu, f'{Vprop_est:.1f} km/d', color='tab:green', ha='left', va='center')
-    
-    if arrow_0:
-        Vprop_est, Vprop_simu = cal_speed(new_list_t, new_list_x, folder, False)
-        start_day = 0.5
-        start_x = 60
-        ax_main.annotate('', 
-            xy=(start_day+1, start_x+10+Vprop_est),
-            xytext=(start_day, start_x+10), 
-            arrowprops=dict(arrowstyle='-|>', color='tab:green', lw=2, linestyle='--', mutation_scale=15)
-        )
-        ax_main.text(start_day+1, start_x+10+Vprop_est, f'{Vprop_est:.1f} km/d', color='tab:green', ha='left', va='center')
-        ax_main.text(0.3, 105, rf'$\epsilon_D = 0$', fontsize=11)
-               
-    if baseline:
-        base_ene = cal_base(folder, coefficient)
-        ax_moment.plot([1,2], [base_ene, base_ene], '--')
+        ax_main.text(start_day+1, start_x+10+Vprop_simu, f'{Vprop_est:.1f} km/d', color='tab:green', ha='left', va='center')   
 
+    return sc
 
+def plot_moment(ax_moment, potency, G, cutoff, start_day, end_day):
+
+    pottime = potency[:, 0]/(24*60*60)
+    potrate = potency[:, 1]
+    pottime -= pottime[0]
+
+    if cutoff:
+        mask_pot = (pottime >= start_day) & (pottime <= end_day)
+        pottime = pottime[mask_pot]
+        potrate = potrate[mask_pot]
+    else:
+        start_day = 0
+        end_day = pottime[-1]
+        print('end_day', end_day)
+
+    ene_level = int(np.log10(G*1e6*max(potrate)))
+    print(ene_level)
+
+    coefficient = G*1e6/10**ene_level
     ax_moment.plot(pottime, potrate*coefficient, color='black', linewidth=0.5)
     ax_moment.set_xlim(start_day, end_day)
     y_max = np.ceil(np.max(potrate)*coefficient)
+    print('y_max', y_max)
     ax_moment.set_ylim(0, y_max)
     ax_moment.set_xlabel('Time (days)')
     ax_moment.spines['top'].set_visible(False)
     ax_moment.spines['right'].set_visible(False)
 
-    ax_moment.set_yticks([0, y_max//3, y_max//3*2])
+    ax_moment.set_yticks([0, (y_max*10//3)*0.1, (y_max*10//3)*0.2])
     ax_moment.set_ylabel(r'$\dot{M_0}$ $(Nm/s)$')
-    ax_moment.text(0, y_max//3*2, fr'$10^{{{ene_level}}}$', fontsize=10, ha='left', va='bottom')
+    ax_moment.text(start_day, y_max/3*2, fr'$10^{{{ene_level}}}$', fontsize=10, ha='left', va='bottom')
     ax_moment.ticklabel_format(useOffset=False)
 
     ax_moment.set_xlabel('Time (days)', fontsize=11)
 
-    return sc
+def plot_alg(iev, folder, begin_days, G, time_window, dis_window, normalize):
+    event_history = np.load(folder + f'/history_X_{iev}.npy')
+    potency = np.load(folder + f'/potency_{iev}.npy')
 
-
-if __name__ == "__main__":
-
-    folder = '../results/test_RSQSim/test_v2.1/'
-
-    params = json.load(open(folder + 'parameters.json'))
-    G, ny, dx = params['region']['G'], params['region']['my'], params['region']['dx']
-    time_window, dis_window = params['model']['time_res'], params['model']['space_res']
-    normalize = time_window*dis_window/dx*ny
-    print(normalize)
-
-    iev = 0
-    ene = 11
     fig = plt.figure(figsize=(10, 4))
     ax_main = fig.add_axes([0.08, 0.24, 0.8, 0.72])
     ax_moment = fig.add_axes([0.08, 0.12, 0.8, 0.12])
-    vmax = 100
-    sc = plot_event(ax_main, ax_moment, folder, iev, vmax, time_window, dis_window, normalize, ene, arrow=False, arrow_0=False, baseline=False)
+    cutoff = False
+    start_day = 616
+    end_day = 620
+    sc = plot_event(ax_main, event_history, time_window, dis_window, normalize, arrow=False, cutoff=cutoff, start_day=start_day, end_day=end_day)
+    plot_moment(ax_moment, potency, G, cutoff=cutoff, start_day=start_day, end_day=end_day)
     
     # plot the colormap
     cbar_ax = fig.add_axes([0.9, 0.25, 0.02, 0.72])
     cbar = fig.colorbar(sc, cax=cbar_ax)
     cbar.set_label(r'Faction of slip activity')
 
-    plt.savefig(folder + f'events/event_{iev}_pick.png')
+    if cutoff:
+        plt.savefig(folder + f'/event_{start_day}_{end_day}.jpg', dpi=300)
+    else:
+        plt.savefig(folder + f'/event_{iev}_startat_{begin_days}_days.jpg', dpi=300)
     plt.close('all')
+
+if __name__ == "__main__":
+
+    # folder = '../results/test_RSQSim/test_v2.2/'
+    folder = '../results/for_comp_qsdm/nx900_my5_eps0.9/'
+    folder_event = 'events'
+    events = pd.read_csv(folder + folder_event + '.csv')
+
+    event_ids = sorted(
+        int(f.stem.split("_")[-1])
+        for f in Path(folder).glob(folder_event + "/history_X_*.npy")
+    )
+
+    params = json.load(open(folder + 'parameters.json'))
+    G, ny, dx = params['region']['G'], params['region']['my'], params['region']['dx']
+    time_window, dis_window = 60, 500#params['model']['time_res'], params['model']['space_res']
+    normalize = time_window*dis_window/dx*ny
+    print(normalize)
+
+    # for iev in event_ids:
+    #     begin_days = int(events.iloc[iev]['start_time'] / (24 * 3600))
+    #     plot_alg(iev, folder + folder_event, begin_days, G, time_window, dis_window, normalize)
+
+    iev = 50
+    # begin_days = int(events.iloc[iev]['start_time'] / (24 * 3600))
+    begin_days = 0
+    plot_alg(iev, folder + folder_event, begin_days, G, time_window, dis_window, normalize)
